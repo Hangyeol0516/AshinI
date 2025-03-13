0977e7302d0138d546f3c999fef17ea304028017
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-03-13 14:59:36 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int N;
+    scanf("%d", &N);
+
+    int result=1;
+    for (int i=1; i<=N; i++) {
+        result = result * i;
+    }
+
+    printf("%d", result);
+    
+    return 0;
+}
