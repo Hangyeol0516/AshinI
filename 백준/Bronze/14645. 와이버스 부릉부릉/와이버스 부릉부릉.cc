db22e8cfdbc7c0c8ad1ceae41eecd7499edeba7d
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-03-27 16:02:19 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    
+    int N, K;
+    std::cin >> N;
+    std::cin >> K;
+
+    for (int i = 0; i < N; i++)
+    {
+        int A, B;
+        std::cin >> A;
+        std::cin >> B;
+    }
+
+    std::cout << "비와이" << std::endl;
+}
