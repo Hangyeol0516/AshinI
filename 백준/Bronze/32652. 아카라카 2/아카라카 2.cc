8faa4dd022b7f88b1f8c18e4ec22eab241568509
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-08 15:00:29 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <array>
+#pragma warning(disable:4996)
+
+int main() {
+
+	int N;
+	scanf("%d", &N);
+
+	printf("AKARAKA");
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		printf("RAKA");
+	}
+
+	return 0;
+}
