44f61bb3a0988fa897896b037eef83c2de7d181c
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-09 14:05:49 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <array>
+#include <algorithm>
+#include <memory>
+#pragma warning(disable:4996) // scanf
+
+int main() {
+
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("LoveisKoreaUniversity ");
+	}
+
+	return 0;
+}
