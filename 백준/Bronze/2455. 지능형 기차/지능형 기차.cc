da11544b10b11474f4eb3ca7bb915614858dd04a
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-16 13:43:59 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#pragma warning(disable:4996) // Visual Studio scanf 오류 방지
+
+int main() {
+
+	int max = 0, count = 0;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		int out, in;
+		scanf("%d %d", &out, &in);
+
+		count -= out;
+		count += in;
+		
+		if (count > max)
+		{
+			max = count;
+		}
+
+	}
+
+	printf("%d", max);
+
+	return 0;
+}
