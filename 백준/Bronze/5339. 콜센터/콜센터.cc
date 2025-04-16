21ad9c4e2b14a035c1ec59fe97c6e47e2f47d859
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-16 15:11:44 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#pragma warning(disable:4996) // Visual Studio scanf 오류 방지
+
+int main() {
+
+	std::cout << "     /~\\" << std::endl;
+	std::cout << "    ( oo|" << std::endl;
+	std::cout << "    _\\=/_" << std::endl;
+	std::cout << "   /  _  \\" << std::endl;
+	std::cout << "  //|/.\\|\\\\" << std::endl;
+	std::cout << " ||  \\ /  ||" << std::endl;
+	std::cout << "============" << std::endl;
+	std::cout << "|          |" << std::endl;
+	std::cout << "|          |" << std::endl;
+	std::cout << "|          |" << std::endl;
+
+}
