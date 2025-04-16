f0620fd4eb91c49e39f5bd0fd842891a951e2cef
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-16 15:07:04 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#pragma warning(disable:4996) // Visual Studio scanf 오류 방지
+
+int main() {
+
+	std::cout << ".  .   ." << std::endl;
+	std::cout << "|  | _ | _. _ ._ _  _" << std::endl;
+	std::cout << "|/\\|(/.|(_.(_)[ | )(/." << std::endl;
+
+}
