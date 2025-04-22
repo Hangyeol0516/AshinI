3c194e82907a0ffe58b337b4b4abadd42b684b44
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-22 12:50:13 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 344 ms, Memory: 8180 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#pragma warning(disable:4996) // Visual Studio scanf 오류 방지
+
+int main() {
+
+	int N;
+	scanf("%d", &N);
+
+	std::vector<int> vector;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		scanf("%d", &num);
+		vector.push_back(num);
+	}
+
+	std::sort(vector.begin(), vector.end());
+
+	for (auto itr = vector.begin(); itr != vector.end(); ++itr)
+	{
+		printf("%d\n", *itr);
+	}
+
+	return 0;
+}
