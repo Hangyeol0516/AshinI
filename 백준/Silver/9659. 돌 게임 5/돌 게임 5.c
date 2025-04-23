745841d413e375fa609d551983c535f62d5a4123
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-23 14:13:39 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    long long N;
+    scanf("%lld", &N);
+
+    if (N % 2 == 0) {
+        printf("CY");
+    }
+    else {
+        printf("SK");
+    }
+
+    return 0;
+}
