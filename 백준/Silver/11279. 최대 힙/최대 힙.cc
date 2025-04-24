21ca116eb22fd1bb5159463a37398087f9265840
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2025-04-24 09:24:14 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <queue> 
+#pragma warning(disable:4996) // Visual Studio scanf 오류 방지
+
+int main() {
+
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	int N;
+	std::cin >> N;
+
+	std::priority_queue<int> pq;
+
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		std::cin >> x;
+
+		if (x == 0)
+		{
+			if (pq.empty())
+			{
+				std::cout << "0\n";
+			}
+			else
+			{
+				std::cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+
+	return 0;
+}
